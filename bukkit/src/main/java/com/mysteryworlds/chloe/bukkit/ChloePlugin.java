4e074a29e8e46cf8714db7b2954bb3b10f7fdb57
Felix Klauke <info@felix-klauke.de>
2019-06-23 13:00:31 +0200

    Add spigot plugin infrastructure.

--- bukkit/src/main/java/com/mysteryworlds/chloe/bukkit/ChloePlugin.java
+++ bukkit/src/main/java/com/mysteryworlds/chloe/bukkit/ChloePlugin.java
@@ -1,5 +1,16 @@
 package com.mysteryworlds.chloe.bukkit;
 
-public class ChloePlugin {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class ChloePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
 }
