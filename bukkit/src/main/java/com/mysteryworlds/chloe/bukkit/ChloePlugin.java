c03a9d62705f1fe3bdd872c3a067f43076cc6d9d
Felix Klauke <info@felix-klauke.de>
2019-06-23 13:11:29 +0200

    Register vault on startup.

--- bukkit/src/main/java/com/mysteryworlds/chloe/bukkit/ChloePlugin.java
+++ bukkit/src/main/java/com/mysteryworlds/chloe/bukkit/ChloePlugin.java
@@ -1,5 +1,8 @@
 package com.mysteryworlds.chloe.bukkit;
 
+import com.mysteryworlds.chloe.bukkit.vault.VaultEconomy;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ChloePlugin extends JavaPlugin {
@@ -7,7 +10,14 @@ public class ChloePlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
+        // Register vault
+        if (getServer().getPluginManager().isPluginEnabled("Vault")) {
 
+            getLogger().info("Vault found. Hooking into vault Economy API.");
+
+            VaultEconomy economy = new VaultEconomy(this);
+            getServer().getServicesManager().register(Economy.class, economy, this, ServicePriority.Highest);
+        }
     }
 
     @Override
